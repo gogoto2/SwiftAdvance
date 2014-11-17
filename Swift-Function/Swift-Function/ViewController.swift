c345b5d7ece6058d868a597007b5cbd6ef4757e9
王钱钧 <arthur.wang@yoho.cn>
2014-11-17 10:46:51 +0800

    添加项目
    学习Swift 函数

---
+++ Swift-Function/Swift-Function/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Function
+//
+//  Created by 王钱钧 on 14/11/17.
+//  Copyright (c) 2014年 王钱钧. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
