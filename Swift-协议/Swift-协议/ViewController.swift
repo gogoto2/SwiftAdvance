08f0a4aedd3760a5935e9c78a7179f5290574873
王钱钧 <arthur.wang@yoho.cn>
2014-11-19 16:47:34 +0800

    添加协议项目代码
    学习Swift 协议

---
+++ Swift-协议/Swift-协议/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Swift-协议
+//
+//  Created by 王钱钧 on 14/11/19.
+//  Copyright (c) 2014年 王钱钧. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+//===============================1 协议的语法 Protocol syntax
+//===============================2 对属性的规定 Propert Requirement
+//===============================3 对方法的规定 Method Requirement
+//===============================4 对突变方法的规定 Mutating Method Requirement
+//===============================5 对构造器的规定 Initializer Requirement
+//===============================6 协议类型 Protocol as Types
+//===============================7 委托模式 Delegation
+//===============================8 在扩展中添加协议成员 Adding Protocol Adoption With an Extension
+//===============================9 通过扩展补充协议声明 Declaring Protocol Adoption With an Extension
+//===============================10 集合中协议类型 Collections of Protocol Types
+//===============================11 协议的继承 Protocol Inheritance
+//===============================12 类专属协议 Class-Only Protocol
+//===============================13 协议合成 Protocol Composition
+//===============================14 检验协议的一致性 Checking for Protocol Conformance
+//===============================15 对可选协议的规定 Optional Protocol Requirement
+
