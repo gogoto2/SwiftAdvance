e7d07ec0ae07ac37e4c1300f95c958e9724c2b82
wangqianjun <wangqianjun@wangqianjundeMacBook-Pro.local>
2014-11-19 23:28:29 +0800

    修改代码，添加一些逻辑

--- Swift-协议/Swift-协议/ViewController.swift
+++ Swift-协议/Swift-协议/ViewController.swift
@@ -79,8 +79,82 @@ func propertRequirement() {
 
 
 //===============================3 对方法的规定 Method Requirement
+/*
+实例可以要求遵循者实现某些指定的实例方法或类方法，这些方法作为协议的一部分，像普通的方法一样清晰的放在协议的定义中，而不需要大括号和方法体
+
+主意：
+协议中得方法支持变长参数 variadic parameter， 不支持默认参数 default value
+
+*/
+
+protocol RandomNumberGenerator {
+    func random() -> Double // 协议并不在意每一个随机数怎样生成的，它只强调这里有一个随机数生成器
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 129968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c) % m)
+        return lastRandom
+    }
+}
+
+
 //===============================4 对突变方法的规定 Mutating Method Requirement
+/*
+主意：
+用类实现协议中的mutating方法时，不需要写mutating关键字
+用结构体和枚举实现协议中得mutating方法时，必须写mutating关键字
+
+*/
+
+protocol Toggle {
+    mutating func toggle()
+}
+
+//当使用枚举或结构体来实现Toggle协议时，需要提供一个带有mutating前缀的toggle方法
+enum OnOffSwitch: Toggle {
+    case Off, On
+    mutating func toggle() {
+        switch self {
+        case .Off:
+            self = .On
+        case .On:
+            self = .Off
+        }
+    }
+}
+
+func mutatingMethodRequirement() {
+    var lightSwitch = OnOffSwitch.Off
+    lightSwitch.toggle()
+}
+
 //===============================5 对构造器的规定 Initializer Requirement
+/*
+协议可以要求它的遵循类型实现特定的构造器
+*/
+protocol SomeProtocol {
+    init(someParameter: Int) // 没有方法体
+}
+
+class SomeSuperClass: SomeProtocol {
+    required init(someParameter: Int) { // 使用required可以保证：所有的遵循该协议的子类，同样能为构造器规定提供一种显示的实现或继承实现
+        // 构造器的实现
+    }
+}
+
+// 如果一个子类重写了父类的制定构造器，并且该构造器遵循了某个协议的规定，那么该构造器的实现需要被同时标示require 和 override
+class SomeSubClass: SomeSuperClass, SomeProtocol {
+    required init(someParameter: Int) {
+        //...
+        super.init(someParameter: someParameter)
+    }
+}
+
 //===============================6 协议类型 Protocol as Types
 //===============================7 委托模式 Delegation
 //===============================8 在扩展中添加协议成员 Adding Protocol Adoption With an Extension
